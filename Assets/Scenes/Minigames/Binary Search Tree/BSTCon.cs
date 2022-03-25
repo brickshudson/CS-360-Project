68325465c3650ef266b9b93c1b8ce6038d7eea21
The-Architect01 <bircng01@pfw.edu>
2022-03-25 12:05:01 -0400

    Made framework for Binary Search tree minigame

---
+++ Assets/Scenes/Minigames/Binary Search Tree/BSTCon.cs
@@ -0,0 +1,124 @@
+//Written by The-Architect01
+using BinaryTree;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class BSTCon : MonoBehaviour {
+
+    public TextMeshProUGUI LeftText;
+    public TextMeshProUGUI RightText;
+    public TextMeshProUGUI CurrentText;
+    public TextMeshProUGUI GoalText;
+    public static BinaryTree<int> BinTree;
+    public static int Goal;
+
+    public static Node<int> CurrentNode = null;
+
+    public bool IsRight = false;
+
+    private void Awake() {
+        BinTree = new BinaryTree<int>();
+        List<int> Goals = new List<int>();
+        while(BinTree.Count < 100) {
+            int x = Random.Range(1, 999);
+            BinTree.Insert(x);
+            Goals.Add(x);
+        }
+        Goal = Goals[Random.Range(0, Goals.Count)];
+        Debug.Log(BinTree);
+        Debug.Log(Goal);
+        CurrentText.text = BinTree.Root.Data.ToString();
+        LeftText.text = BinTree.Root.Left.Data.ToString();
+        RightText.text = BinTree.Root.Right.Data.ToString();
+        CurrentNode = BinTree.Root;
+    }
+
+    public void OnEnter() {
+        GetComponent<Image>().color = new Color(255, 255, 255, 255);
+    }
+    public void OnExit() {
+        GetComponent<Image>().color = new Color(255, 255, 255, 0);
+    }
+
+    public void OnClick() {
+        if (IsRight) {
+            if(RightText.text == Goal.ToString()) { Debug.Log("Correct Right"); return; }
+            if(CurrentNode.Right != null)
+                CurrentNode = CurrentNode.Right;
+        } else {
+            if (LeftText.text == Goal.ToString()) { Debug.Log("Correct Left"); return; }
+            if (CurrentNode.Left != null)
+                CurrentNode = CurrentNode.Left;
+        }
+        CurrentText.text = CurrentNode.Data.ToString();
+        try {
+            LeftText.text = CurrentNode.Left.Data.ToString();
+        } catch { LeftText.text = ""; }
+        try {
+            RightText.text = CurrentNode.Right.Data.ToString();
+        }catch { RightText.text = ""; }
+        Debug.Log("Clicked");
+    }
+}
+namespace BinaryTree {
+
+    public class Node<T> where T : System.IComparable<T> {
+        public T Data { get; set; }
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+        
+        public Node(T data, Node<T> Left, Node<T> Right) {
+            Data = data;
+            this.Left = Left;
+            this.Right = Right;
+        }
+    }
+
+    public class BinaryTree<T> where T : System.IComparable<T> {
+        public Node<T> Root { get; private set; } = null;
+        public int Count { get { return CountNodes(Root); } }
+
+        public void Insert(T data) {
+            if(Root == null) {
+                Root = new Node<T>(data, null, null);
+            } else {
+                InsertSort(Root, new Node<T>(data,null,null));
+            }
+        }
+        
+        void InsertSort(Node<T> Base, Node<T> InsertValue) {
+            if(InsertValue.Data.CompareTo(Base.Data) < 0) {
+                if(Base.Left == null) {
+                    Base.Left = InsertValue;
+                } else {
+                    InsertSort(Base.Left, InsertValue);
+                }
+            } else { 
+                if(Base.Right == null) {
+                    Base.Right = InsertValue;
+                } else {
+                    InsertSort(Base.Right, InsertValue);
+                }
+            }
+        }
+
+        int CountNodes(Node<T> Root) {
+            if(Root == null) { return 0; }
+            return 1 + CountNodes(Root.Left) + CountNodes(Root.Right);
+        }
+
+        string Tostring(Node<T> node) {
+            if(node == null) { return string.Empty; }
+            return Tostring(node.Left) + " " + node.Data.ToString() + " " + Tostring(node.Right);
+        } 
+
+        public override string ToString() {
+            return Tostring(Root);
+        }
+
+    }
+
+}
