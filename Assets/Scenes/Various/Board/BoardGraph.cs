bbe63e001bf9634d936997350991f180a90a4f0e
Tyler Delagrange <tjdelagrange@gmail.com>
2022-03-28 18:28:18 -0700

    started work on creating final board graph

--- Assets/Scenes/Various/Board/BoardGraph.cs
+++ Assets/Scenes/Various/Board/BoardGraph.cs
@@ -12,11 +12,18 @@ public class BoardGraph //Note - for testing purposes this is NOT a monobehavior
     //Ex: A space with 2 connections, left and right = new GameSpace(20, "Type", -1, -1, 19, 21, x, y)
     void Start()
     {}//As of now we don't need this method
+
+    //I lied when I said the GameSpace constructor was the worst, this is MUCH WORSE! >:D
     public BoardGraph()
     {
         routes[0] = new GameSpace(0, "Green", -1, -1, 2, 1, 0f, 0f);
         routes[1] = new GameSpace(1, "Blue" , -1, -1, 0, 3, 3.05f, 0.64f);
-        routes[2] = new GameSpace(2, "Red"  , -1, -1, 4, 0, -3.28f, 1.17f);
+        routes[2] = new GameSpace(2, "Red"  , 4, -1, 8, 0, -3.28f, 1.17f);
+        routes[3] = new GameSpace(3, "Blue" , 5, -1, 1, -1, 5.23f, 1.87f);
+        routes[4] = new GameSpace(4, "Blue" , 0, 2, -1, 6, -1.88f, 4.18f);
+        routes[5] = new GameSpace(5, "Red"  , 7, 3, 6, -1, 4.68f, 3.58f);
+        routes[6] = new GameSpace(6, "Green", -1, -1, 4, 5, 1.53f, 3.52f);
+        routes[7] = new GameSpace(7, "Shop", -1, 5, -1, -1, 5.07f, 5.27f);
     }
     
     public GameSpace get(int i)
