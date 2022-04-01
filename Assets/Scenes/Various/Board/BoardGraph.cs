ace3d7d07da72f300f9c16e8a462435778d62af4
Tyler Delagrange <tjdelagrange@gmail.com>
2022-04-01 16:42:24 -0700

    more graph stuff

--- Assets/Scenes/Various/Board/BoardGraph.cs
+++ Assets/Scenes/Various/Board/BoardGraph.cs
@@ -16,6 +16,7 @@ public class BoardGraph //Note - for testing purposes this is NOT a monobehavior
     //I lied when I said the GameSpace constructor was the worst, this is MUCH WORSE! >:D
     public BoardGraph()
     {
+        //All of this is just constructing the game spaces and building the graph with them.
         routes[0]  = new GameSpace(0, "Green", -1, -1, 2, 1, 0f, 0f);
         routes[1]  = new GameSpace(1, "Blue" , -1, -1, 0, 3, 3.05f, 0.64f);
         routes[2]  = new GameSpace(2, "Red"  , 4, -1, 8, 0, -3.28f, 1.17f);
@@ -30,18 +31,18 @@ public class BoardGraph //Note - for testing purposes this is NOT a monobehavior
         routes[11] = new GameSpace(11, "Boat", -1, 10, -1, -1, -12.75f, 3.97f);
         routes[12] = new GameSpace(12, "Green", 15, 4, 13, 14, -3.15f, 7.34f);
         routes[13] = new GameSpace(13, "Red", 16, 8, -1, 12, -6.57f, 6.77f);
-        routes[14] = new GameSpace(14, "Blue", -1, -1, 12, 0, -0.21f, 8.89f);
+        routes[14] = new GameSpace(14, "Blue", -1, -1, 12, 20, -0.21f, 8.89f);
         routes[15] = new GameSpace(15, "Green", 17, 12, -1, -1, -3.39f, 11.08f);
-
         routes[16] = new GameSpace(16, "Blue", 17, 13, -1, -1, -7.13f, 9.78f);
         routes[17] = new GameSpace(17, "Blue", -1, 15, 16, 18, -3.98f, 13.91f);
-        routes[18] = new GameSpace(18, "Red", 4, -1, 18, 0, -.49f, 14.02f);
-        routes[19] = new GameSpace(19, "Blue", 5, -1, 1, -1, 5.23f, 1.87f);
-        routes[20] = new GameSpace(20, "Blue", 12, 2, -1, 6, -1.88f, 4.18f);
-        routes[21] = new GameSpace(21, "Red", 7, 3, 6, -1, 4.68f, 3.58f);
-        routes[22] = new GameSpace(22, "Green", -1, -1, 4, 5, 1.53f, 3.52f);
-        routes[23] = new GameSpace(23, "Shop", -1, 5, -1, -1, 5.07f, 5.27f);
-        routes[24] = new GameSpace(24, "Green", 13, -1, 9, 2, -6.28f, 2.64f);
+        routes[18] = new GameSpace(18, "Green", 4, -1, 17, 19, -.49f, 14.02f);
+        routes[19] = new GameSpace(19, "Green", -1, 20, 18, -1, 3.12f, 13.48f);
+        routes[20] = new GameSpace(20, "Blue", 19, -1, 14, 21, 3.24f, 10.18f);
+        routes[21] = new GameSpace(21, "Red", -1, -1, 20, 22, 7.38f, 10.27f);
+        routes[22] = new GameSpace(22, "Red", -1, -1, 21, 23, 11.49f, 10.3f);
+
+        routes[23] = new GameSpace(23, "Blue", 24, 22, -1, -1, 14.21f, 12.43f);
+        routes[24] = new GameSpace(24, "Red", 25, 23, -1, -1, 14.5f, 16.05f);
         routes[25] = new GameSpace(25, "Red", -1, -1, 10, 8, -8.9f, 1.18f);
         routes[26] = new GameSpace(26, "Red", 11, -1, -1, 9, -12.98f, 1.57f);
         routes[27] = new GameSpace(27, "Boat", -1, 10, -1, -1, -12.75f, 3.97f);
