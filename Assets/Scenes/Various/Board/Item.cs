15e5c27688afce6463b14522d967bc20aaa12e9b
Tyler Delagrange <tjdelagrange@gmail.com>
2022-04-08 17:38:08 -0700

    added an item parent class

---
+++ Assets/Scenes/Various/Board/Item.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+    string name;
+    string description;
+    Sprite sprite;
+    SpriteRenderer spriteRenderer; // I don't know whether I need this or not yet, but it's nice to have! :D
+
+    public void use()
+    {
+        
+    }
+}
