34dd16c48d2f8338fb6d3c5e9405921ad188555b
Bricks <danh2014@gmail.com>
2022-04-08 22:54:11 -0400

    added interpolation to camera on the board

--- Assets/cameraFollow.cs
+++ Assets/cameraFollow.cs
@@ -6,9 +6,23 @@ public class cameraFollow : MonoBehaviour
 {
     public Transform target;
     public Vector2 offSet;
+    public float speedMultiplier = 10f;
+    public float speedCap = 25.0f;
 
     private void Update()
     {
-        transform.position = target.position;
+        Vector3 distance = (target.position - transform.position);
+
+        if(distance.magnitude > 0.01 && distance.magnitude < 10000)
+        {
+            if(speedMultiplier * distance.magnitude < speedCap)
+                distance = speedMultiplier * distance.magnitude * distance.normalized;
+            else
+                distance = speedCap * distance.normalized;
+
+            transform.position = transform.position + Time.deltaTime * distance;
+        }
+        else
+            transform.position = target.position;
     }
 }
